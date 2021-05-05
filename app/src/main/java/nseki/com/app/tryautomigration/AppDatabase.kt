771c621df5389edae2e2d491a6e6c8a888382d56
n-seki <developer.n.seki@gmail.com>
2021-05-05 16:18:05 +0900

    migration from 1 to 2

--- app/src/main/java/nseki/com/app/tryautomigration/AppDatabase.kt
+++ app/src/main/java/nseki/com/app/tryautomigration/AppDatabase.kt
@@ -1,13 +1,17 @@
 package nseki.com.app.tryautomigration
 
+import androidx.room.AutoMigration
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
 @Database(
-    version = 1,
+    version = 2,
     exportSchema = true,
     entities = [
         Books::class
+    ],
+    autoMigrations = [
+        AutoMigration(from = 1, to = 2)
     ]
 )
 abstract class AppDatabase : RoomDatabase()
--- app/src/main/java/nseki/com/app/tryautomigration/Books.kt
+++ app/src/main/java/nseki/com/app/tryautomigration/Books.kt
@@ -1,10 +1,12 @@
 package nseki.com.app.tryautomigration
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity
 data class Books(
     @PrimaryKey val title: String,
-    val author: String
+    val author: String,
+    @ColumnInfo(defaultValue = "0") val page: Int
 )
